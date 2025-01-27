b8aef64ac0d1cffcea04e5b1254a848c9ee62a48
Bruno-Diego <bruno.diego.x@hotmail.com>
2025-01-27 17:57:26 +0100

    add contact message

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,66 +1,12 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 // import emailjs from "@emailjs/browser";
 
 import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  const formRef = useRef();
-  const [form, setForm] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const [loading, setLoading] = useState(false);
-
-  const handleChange = (e) => {
-    const { target } = e;
-    const { name, value } = target;
-
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoading(true);
-
-    // emailjs
-    //   .send(
-    //     import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-    //     import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-    //     {
-    //       from_name: form.name,
-    //       to_name: "Bruno Diego da Silva",
-    //       from_email: form.email,
-    //       to_email: "bruno.diego.x@hotmail.com",
-    //       message: form.message,
-    //     },
-    //     import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    //   )
-    //   .then(
-    //     () => {
-    //       setLoading(false);
-    //       alert("Thank you. I will get back to you as soon as possible.");
-
-    //       setForm({
-    //         name: "",
-    //         email: "",
-    //         message: "",
-    //       });
-    //     },
-    //     (error) => {
-    //       setLoading(false);
-    //       console.error(error);
-
-    //       alert("Ahh, something went wrong. Please try again.");
-    //     }
-    //   );
-  };
+  
 
   return (
     <div
@@ -82,54 +28,9 @@ const Contact = () => {
           Contact.
         </h3>
 
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
-        >
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
-            <input
-              type="text"
-              name="name"
-              value={form.name}
-              onChange={handleChange}
-              placeholder="What's your good name?"
-              className="bg-[#272C4A] py-4 px-6 placeholder:text-white text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your email</span>
-            <input
-              type="email"
-              name="email"
-              value={form.email}
-              onChange={handleChange}
-              placeholder="What's your web address?"
-              className="bg-[#272C4A] py-4 px-6 placeholder:text-white text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-          <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Message</span>
-            <textarea
-              rows={7}
-              name="message"
-              value={form.message}
-              onChange={handleChange}
-              placeholder="What you want to say?"
-              className="bg-[#272C4A] py-4 px-6 placeholder:text-white text-white rounded-lg outline-none border-none font-medium"
-            />
-          </label>
-
-          <button
-            type="submit"
-            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-[#18206F] font-bold shadow-md shadow-primary"
-            // Botao disabilitado ate que eu configure corretamente
-            disabled
-          >
-            {loading ? "Sending..." : "Send"}
-          </button>
-        </form>
+        <div>
+          <p>Under development</p>
+        </div>
       </motion.div>
     </div>
   );
